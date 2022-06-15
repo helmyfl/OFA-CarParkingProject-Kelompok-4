5ddf0d58c52200eb56dd2569a35328c048463d44
helmyfl <helmyefel@students.unnes.ac.id>
2022-06-15 12:51:57 +0700

    Ïnital commit

---
+++ review/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import requests
+import sys
+from subprocess import run, PIPE
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def external(request):
+    inp = request.POST.get('param')
+    out = run([sys.executable, 'D:\KULIAH\MAGANG\PA FIXXXXXXXXXXXXX\CarParkProject\main.py', inp], shell=False,
+              stdout=PIPE)
+    print(out)
+
+    return render(request, 'index.html', {'data1': out.stdout.decode('utf-8')})
